ac7a71e0bfef3b5251d0f8698d5a78d254a92483
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-08-16 18:57:08 -0400

    heroku

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
